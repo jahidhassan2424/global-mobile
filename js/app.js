4dcf58ecbf538cc25bf3fb9c5a5d76ee63ab3ccc
jahidhassan2424 <jahidhassan.programmer@gmail.com>
2022-03-01 14:49:44 +0600

    Nothing Found Added

--- js/app.js
+++ js/app.js
@@ -8,6 +8,11 @@ const create = name => {
     return document.createElement(name);
 }
 
+// Clear 
+const clear = name => {
+    return get(name).textContent = '';
+}
+
 const loadAllMobile = searchText => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
@@ -20,28 +25,30 @@ const searchButton = () => {
     const searchText = get('search-field').value;
     document.getElementById('showing-search-result-for').innerHTML = `Showing search result for <span ></span>"${searchText}"`;
     get('search-field').value = '';
-    const output = get('output-result');
-    output.textContent = '';
+    clear('output-result');
     loadAllMobile(searchText);
-
 }
 
 const showAllResult = data => {
-
     const phones = data.data
-    console.log(data.data);
-    // console.log(data.data.brand);
-
     let count = data.data.length
+
     if (count === 0) {
-        const h1 = create('h1');
-        h1.innerText = "No Result Found";
-        const output = get('output');
-        output.appendChild(h1);
+
+
+        clear('nothing-found');
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div style="text-align:center" >
+        <img src="image/no-result-found.png">
+        <div>
+        `;
+        get('nothing-found').appendChild(div);
         console.log('No result Found');
 
     }
     else if (count >= 20) {
+        clear('nothing-found');
         // phones.forEach(element => {
         for (let i = 0; i < 20; i++) {
             let count = 0;
@@ -56,7 +63,6 @@ const showAllResult = data => {
                         <p class="">Brand: ${data.data[i].brand} </p>
                         <a href="#"><button class="btn btn-primary fw-bold ">Details</button></a>
                     </div>
-                    
                 </div>
             `;
                 const output = get('output-result');
@@ -65,11 +71,9 @@ const showAllResult = data => {
             }
         }
     }
-
     else {
-        // console.log(data.data[0].phone_name);
+        clear('nothing-found');
         phones.forEach(element => {
-            // console.log(element.phone_name);
             const div = document.createElement('div');
             div.classList = 'col col-4';
             div.innerHTML = `
@@ -80,22 +84,11 @@ const showAllResult = data => {
                         <p class="">Brand: ${element.brand} </p>
                         <a href="#"><button class="btn btn-primary fw-bold ">Details</button></a>
                     </div>
-                    
                 </div>
             `;
-
             const output = get('output-result');
             output.appendChild(div);
-
-            // const output = get('output');
-            // output.appendChild(div);
-            // console.log('Result > 0');
-        }
-
-        );
+        })
     }
-    // return count;
 }
-
-
 
